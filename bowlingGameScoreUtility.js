5dca76ccec31550e8bf650bf44d3fe778131a8ae
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-18 12:55:40 +0530

    fix: corrected the logic to work on wider types of cases

--- bowlingGameScoreUtility.js
+++ bowlingGameScoreUtility.js
@@ -8,60 +8,37 @@ const scoreCalculator=(pinsDownArray)=>{
             if(pinsDownArray[i]==10){
                 totalScore+=10;
                 i+=1;
-                if(i<pinsDownArray.length){
-                    if(pinsDownArray[i]==10){
-                        totalScore+=10
-                        i+=1;
-                        if(pinsDownArray[i]==10){
-                            i+=1;
-                            totalScore+=10;
-                        }
-                    }else if(i+1<pinsDownArray.length && pinsDownArray[i]+pinsDownArray[i+1]==10){
-                        totalScore+=10;
-                    }
-                }
-                currentFrameScore=10+pinsDownArray[i+1]+pinsDownArray[i+2];
-                frames.push([10, pinsDownArray[i+1], pinsDownArray[i+2]])
-            }else if(pinsDownArray[i]+pinsDownArray[i+1]==10){
-                if(i<pinsDownArray.length){
-                    if(pinsDownArray[i]==10){
-                        totalScore+=10
-                    }else if(i+1<pinsDownArray.length && pinsDownArray[i]+pinsDownArray[i+1]==10){
-                        totalScore+=10;
-                    }
-                }
-                currentFrameScore=10+pinsDownArray[i+2];
-                i+=1;
-                frames.push([pinsDownArray[i], pinsDownArray[i+1], pinsDownArray[i+2]])
+                // currentFrameScore=10+pinsDownArray[i+1]+pinsDownArray[i+2];
+                // frames.push([10, pinsDownArray[i+1], pinsDownArray[i+2]])
             }else{
-                if(i<pinsDownArray.length){
-                    if(pinsDownArray[i]==10){
-                        totalScore+=10
-                    }else if(i+1<pinsDownArray.length && pinsDownArray[i]+pinsDownArray[i+1]==10){
-                        totalScore+=10;
-                    }
+                if(i+1<pinsDownArray.length){
+                    totalScore+=10;
+                    i+=1;
                 }
-                currentFrameScore=pinsDownArray[i]+pinsDownArray[i+1];
-                i+=1;
-                frames.push([pinsDownArray[i], pinsDownArray[i+1]])
+                // currentFrameScore=10+pinsDownArray[i+2];
+                // i+=1;
+                // frames.push([pinsDownArray[i], pinsDownArray[i+1], pinsDownArray[i+2]])
             }
-            break;
         }
         if(pinsDownArray[i]==10){
             currentFrameScore=10+pinsDownArray[i+1]+pinsDownArray[i+2];
             frames.push([10, pinsDownArray[i+1], pinsDownArray[i+2]])
+            frameCount+=1;
         }else if(i+2<pinsDownArray.length&&pinsDownArray[i]+pinsDownArray[i+1]==10){
             currentFrameScore=10+pinsDownArray[i+2];
             i+=1;
             frames.push([pinsDownArray[i], pinsDownArray[i+1], pinsDownArray[i+2]])
+            frameCount+=1;
         }else if(i+1<pinsDownArray.length){
             currentFrameScore=pinsDownArray[i]+pinsDownArray[i+1];
             i+=1;
             frames.push([pinsDownArray[i], pinsDownArray[i+1]])
+            frameCount+=1;
         }
         totalScore+=currentFrameScore;
-        frameCount+=1;
+        
     }
+    if(frameCount!=10) throw new Error('Number of frames more than 10');
     return [frameCount, frames, totalScore]
 }
-console.log(scoreCalculator([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]))
+console.log(scoreCalculator([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
